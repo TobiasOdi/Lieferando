d025cefdb3a813c12f4da639c221988a6b7e1bdd
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2023-01-19 16:58:57 +0100

    foots section designed

---
+++ script.js
@@ -0,0 +1,34 @@
+
+let foods = [
+
+{   'categorie': 'Beliebt',
+    'title': 'Mi Ga Chien Gion',
+    'description': 'gebratene Nudeln mit paniertem Hühnerfleisch, Gemüse und Erdnusssauce',
+    'price': '12,90'
+},
+
+{   'categorie': 'Beliebt',
+    'title': 'Mi Ga Chien Gion',
+    'description': 'gebratene Nudeln mit paniertem Hühnerfleisch, Gemüse und Erdnusssauce',
+    'price': '12,90'
+},
+
+{   'categorie': 'Sushi Menüs',
+    'title': 'Menü (für zwei Personen)',
+    'description': '2 Miso Soup, 8 Sake inside outs, 8 Sake Rolls, 8 Kani Rolls, 6 Sake Maki, 6 Clifornia Maki, 2 Sake Nigiri, 2 Maguro Nigiri',
+    'price': '47,90'
+},
+
+{   'categorie': 'Sushi Menüs',
+    'title': 'Menü (für zwei Personen)',
+    'description': '2 Miso Soup, 8 Sake inside outs, 8 Sake Rolls, 8 Kani Rolls, 6 Sake Maki, 6 Clifornia Maki, 2 Sake Nigiri, 2 Maguro Nigiri',
+    'price': '47,90'
+}
+]; 
+
+function render() {
+
+
+
+
+}
