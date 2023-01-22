4560a32064a325bdf2767a39162449b7d2508258
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2023-01-22 20:54:26 +0100

    basket design

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/* =================================== RENDER FOODS ======================================= */
 let foods = [
 
 {   'categorie': 'Beliebt',
@@ -28,24 +28,26 @@ let foods = [
 
 function render() {
 
-document.getElementById('content').innerHTML = '';
-
 for (let i = 0; i < foods.length; i++) {
-
+    let dishCategorie = foods[i]['categorie']
     let dishTitle = foods[i]['title'];
     let dishDescription = foods[i]['description'];
     let dishPrice = foods[i]['price'];
 
-    
-    document.getElementById('content').innerHTML += renderTemplate(i);
+    let contentFavorites = document.getElementById('favorites').innerHTML;
+    let sushiMenues = document.getElementById('sushiMenues').innerHTML;
+
+  if(dishCategorie.includes('Beliebt')) {
+    document.getElementById('favorites').innerHTML += renderTemplate(i);
+
+  } else if(dishCategorie.includes('Sushi Menüs')) {
+    document.getElementById('sushiMenues').innerHTML += renderTemplate(i);
+  }
 }
 }
 
-function renderTemplate() {
+function renderTemplate(i) {
     return `
-    <div>
-            <div class="categorie">
-              <h1>Beliebt</h1>
               <div>
                 <div>
                   <div>
@@ -53,7 +55,7 @@ function renderTemplate() {
                     <img onclick="openInfo()" src="./img/icons/information (1).png" alt="icon eines Informationssymbols"/>
                   </div>
                   <div>
-                    <img onclick="addDish()" src="./img/icons/plus.png" alt="" />
+                    <img onclick="addToBasekt(${i})" src="./img/icons/plus.png" alt="" />
                   </div>
                 </div>
                 <p>
@@ -63,88 +65,71 @@ function renderTemplate() {
                   <p>${foods[i]['price']}</p>
                   <p>€</p>
                 </div>
-              </div>
-
-              <div>
-                <div>
-                  <div>
-                    <h1>${foods[i]['title']}</h1>
-                    <img onclick="openInfo()" src="./img/icons/information (1).png" alt="icon eines Informationssymbols"/>
-                  </div>
-                  <div>
-                    <img onclick="addDish()" src="./img/icons/plus.png" alt="" />
-                  </div>
-                </div>
-                <p>
-                  ${foods[i]['description']}
-                </p>
-                <div class="price">
-                  <p>${foods[i]['price']}</p>
-                  <p>€</p>
-                </div>
-              </div>
-            </div>
-
-            <div class="categorie">
-              <h1>Sushi Menüs</h1>
-              <div>
-                <div>
-                  <div>
-                    <h1>${foods[i]['title']}</h1>
-                    <img onclick="openInfo()" src="./img/icons/information (1).png" alt="icon eines Informationssymbols"/>
-                  </div>
-                  <div>
-                    <img onclick="addDish()" src="./img/icons/plus.png" alt="" />
-                  </div>
-                </div>
-                <p>
-                  ${foods[i]['description']}
-                </p>
-                <div class="price">
-                  <p>${foods[i]['price']}</p>
-                  <p>€</p>
-                </div>
-              </div>
-
-              <div>
-                <div>
-                  <div>
-                    <h1>${foods[i]['title']}</h1>
-                    <img onclick="openInfo()" src="./img/icons/information (1).png" alt="icon eines Informationssymbols"/>
-                  </div>
-                  <div>
-                    <img onclick="addDish()" src="./img/icons/plus.png" alt="" />
-                  </div>
-                </div>
-                <p>
-                  ${foods[i]['description']}
-                </p>
-                <div class="price">
-                  <p>${foods[i]['price']}</p>
-                  <p>€</p>
-                </div>
-              </div>
-            </div>
-          </div>`;
+              <div>`;
 }
 
-
-
-
-
-
-
-
-
-
+/* =================================== OPEN INFO ======================================= */
 function openInfo() {
 
 
 }
 
-function addDish() {
+/* =================================== ADD DISH TO BASKET ======================================= */
+
+let basket = [];
+let prices = [];
+let amounts = [];
+
+function addToBasekt(food, price) {
+let index = basket.indexOf(food); // Index 0
+if(index == -1) {
+
+} else {
+  amount[index]++
+}
+  
+  names.push(name);
+    
+}
+
+
+function renderBasket() {
+  let container = document.getElementById('container');
+  for (let i = 0; i < basket.length; i++) {
+    const item = basket[i];
+    const price = prices[i];
+    const amount = amounts[i];
+
+    let sum = 0;
+
+    sum += prices[i] * amounts[i];
+
+    container.innerHTML += `
+      <div>
+        ${amount} * ${item}: ${price}
+      </div>
+
+    `;
 
     
+  }
+}
+
+
+
+
+
+function updateBasket () {
+  let sum = 0;
+  for (let i = 0; i < prices.length; index++) {
+    const element = array[index];
+      sum += prices[i];
+}
+
+  document.getElementById('').innerHTML = sum;
+
+  let finalSum = sum + 5;
+
 }
 
 
