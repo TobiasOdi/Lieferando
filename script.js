9a6564f3132950df491d9209a62934c5cb9657a9
Tobias Odermatt <odermatt.tobias.w@gmail.com>
2023-01-26 13:46:39 +0100

    basket finalize

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ let foods = [
 },
 
 {   'categorie': 'Beliebt',
-    'title': 'Mi Ga Chien Gion',
-    'description': 'gebratene Nudeln mit paniertem Hühnerfleisch, Gemüse und Erdnusssauce',
-    'price': 12.90
+    'title': 'Mi Gon',
+    'description': 'gebratene Nudeln mit, Gemüse und Erdnusssauce',
+    'price': 9.90
 },
 
 {   'categorie': 'Sushi Menüs',
-    'title': 'Menü (für zwei Personen)',
+    'title': 'Menü (für 2 Personen)',
     'description': '2 Miso Soup, 8 Sake inside outs, 8 Sake Rolls, 8 Kani Rolls, 6 Sake Maki, 6 Clifornia Maki, 2 Sake Nigiri, 2 Maguro Nigiri',
     'price': 47.90
 },
 
 {   'categorie': 'Sushi Menüs',
-    'title': 'Menü (für zwei Personen)',
-    'description': '2 Miso Soup, 8 Sake inside outs, 8 Sake Rolls, 8 Kani Rolls, 6 Sake Maki, 6 Clifornia Maki, 2 Sake Nigiri, 2 Maguro Nigiri',
-    'price': 47.90
+    'title': 'Menü (für 4 Personen)',
+    'description': '4 Miso Soup, 16 Sake inside outs, 16 Sake Rolls, 16 Kani Rolls, 12 Sake Maki, 12 Clifornia Maki, 4 Sake Nigiri, 4 Maguro Nigiri',
+    'price': 90.00
 }
 ]; 
 
@@ -92,7 +92,6 @@ if(t == -1) {
 } else {
   amounts[t]++;
 }
-
   renderBasket();
   save();
 }
@@ -111,24 +110,41 @@ function renderBasket() {
   basketContent.innerHTML = '';
 
   if(basket.length == 0){
+
+    basketContent += `
+    <div>
+        <h1>Warenkorb</h1>
+        </div>
+        <div>
+          <img
+            src="./img/icons/einkaufstasche.png"
+            alt="Icon einr Einkaufstasche"
+          />
+          <p>Fülle deinen Warenkorb</p>
+          <p>
+            Füge einige leckere Gerichte aus der Speisekarte hinzu und bestelle
+            dein Essen.
+          </p>
+    </div>`;
   } else {
 
-  for (let i = 0; i < basket.length; i++) {
-    const item = basket[i];
-    const price = prices[i];
-    const amount = amounts[i];
+    for (let i = 0; i < basket.length; i++) {
+      const item = basket[i];
+      const price = prices[i];
+      let priceFormatted = price.toFixed(2).replace('.', ',');
+      const amount = amounts[i];
 
-    let sum = 0;
-    sum += prices[i] * amounts[i];
-    let totalSum = sum + 5;
+      let totalItemPrice = prices[i] * amounts[i];
+      let totalItemPriceFormatted = totalItemPrice.toFixed(2).replace('.', ',');
 
-    let diff = 57 - totalSum;
-    //if(diff <= 57) {
-    //} else {
-    //  document.getElementById('hint').classList.add('hide')
-    //};
+      //let sum = 0;
+      //sum += prices[i] * amounts[i];
+      //let totalSum = sum + 5;
 
-    basketContent.innerHTML += `
+      //let diff = 57 - totalSum;
+      //let diffFormatted = diff.toFixed(2).replace('.', ',');
+
+      basketContent.innerHTML += `
     <div id="items" class="items">
       <div id="item" class="item">
         <div>
@@ -138,7 +154,7 @@ function renderBasket() {
               <p>${item}</p>
             </div>
             <div>
-              <p>${price.toFixed(2).replace('.', ',')}</p>
+              <p>${totalItemPriceFormatted}</p>
               <p class="eur">€</p>
             </div>
           </div>
@@ -153,17 +169,51 @@ function renderBasket() {
           </div>
         </div>
       </div>
-    </div>
+    </div>`;
+  }
 
-    <div id="hint" class="hint">
-      <div>Benötigter Betrag, um den Mindestbestellwert zu erreichen</div>
-      <div>
-        <p>${diff.toFixed(2).replace('.', ',')}</p>
-        <p>€</p>
-      </div>
-    </div>
+  renderBasketHint();
+  renderBasektCalculation();
+}
+}
 
-    <div id="calculation" class="calculation">
+function renderBasketHint() {
+  //let sum = 0;
+  //sum += prices[i] * amounts[i];
+  //let totalSum = sum + 5;
+
+
+  let diff = 57 
+  
+  //- totalSum;
+  //if(diff <= 57) {
+  //} else {
+  //  document.getElementById('hint').classList.add('hide')
+  //};
+
+  basketContent.innerHTML += `
+
+  <div id="hint" class="hint">
+    <div>Benötigter Betrag, um den Mindestbestellwert zu erreichen</div>
+    <div>
+      <p>${diff.toFixed(2).replace('.', ',')}</p>
+      <p>€</p>
+    </div>
+  </div>`;
+
+}
+
+function renderBasektCalculation(){
+
+  let sum = 0;
+  //sum += prices[i] * amounts[i];
+
+  sum = prices.reduce((partialSum, a) => partialSum + a, 0);
+
+  let totalSum = sum + 5;
+
+  basketContent.innerHTML += `
+  <div id="calculation" class="calculation">
       <div>
         <div>
           <div>
@@ -189,11 +239,103 @@ function renderBasket() {
         </div>
         <button type="button">Bezahlen (${totalSum.toFixed(2).replace('.', ',')} €)</button>
       </div>
+    </div>`;
+} 
+
+
+
+
+
+/* <div id="hint" class="hint">
+<div>Benötigter Betrag, um den Mindestbestellwert zu erreichen</div>
+<div>
+  <p>${diffFormatted}</p>
+  <p>€</p>
+</div>
+</div>
+
+<div id="calculation" class="calculation">
+<div>
+  <div>
+    <div>
+      <div>
+        <p>Zwischensumme</p>
+      </div>
+      <div>
+        <p>${sum.toFixed(2).replace('.', ',')}</p>
+        <p class="eur">€</p>
+      </div>
     </div>
-    `;
-  }
-}
-}
+    <div>
+      <p>Lieferkosten</p>
+      <p>5.00 €</p>
+    </div>
+    <div>
+      <div>Gesamt</div>
+      <div>
+        <p>${totalSum.toFixed(2).replace('.', ',')}</p>
+        <p class="eur">€</p>
+      </div>
+    </div>
+  </div>
+  <button type="button">Bezahlen (${totalSum.toFixed(2).replace('.', ',')} €)</button>
+</div>
+</div> */
+
+/* function renderBasketHint() {
+  let diff = 57 - totalSum;
+  //if(diff <= 57) {
+  //} else {
+  //  document.getElementById('hint').classList.add('hide')
+  //};
+
+  basketContent.innerHTML = `
+
+  <div id="hint" class="hint">
+    <div>Benötigter Betrag, um den Mindestbestellwert zu erreichen</div>
+    <div>
+      <p>${diff.toFixed(2).replace('.', ',')}</p>
+      <p>€</p>
+    </div>
+  </div>`;
+
+} */
+
+/* function renderBasektCalculation(){
+
+  let sum = 0;
+  sum += prices[i] * amounts[i];
+  let totalSum = sum + 5;
+
+  basketContent.innerHTML = `
+  <div id="calculation" class="calculation">
+      <div>
+        <div>
+          <div>
+            <div>
+              <p>Zwischensumme</p>
+            </div>
+            <div>
+              <p>${sum.toFixed(2).replace('.', ',')}</p>
+              <p class="eur">€</p>
+            </div>
+          </div>
+          <div>
+            <p>Lieferkosten</p>
+            <p>5.00 €</p>
+          </div>
+          <div>
+            <div>Gesamt</div>
+            <div>
+              <p>${totalSum.toFixed(2).replace('.', ',')}</p>
+              <p class="eur">€</p>
+            </div>
+          </div>
+        </div>
+        <button type="button">Bezahlen (${totalSum.toFixed(2).replace('.', ',')} €)</button>
+      </div>
+    </div>`;
+} */
 
 
 
